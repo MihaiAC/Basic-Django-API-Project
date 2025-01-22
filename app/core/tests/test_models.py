8b9caa2696677b0c520a3dd0d586f994feffe19d
MihaiAC <alexmihai.ciobanu@gmail.com>
2025-01-22 20:23:19 +0200

    Create test for basic user model.

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,23 @@
+"""
+Tests for app models
+"""
+
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+    """Test models"""
+
+    # Do we have a separate dummy DB for testing?
+    def test_create_user_with_email_successful(self):
+        """Tests successful creation of user with email."""
+        email = "test@example.com"
+        password = "testpass123"
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password,
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
