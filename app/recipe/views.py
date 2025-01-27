a0b9c97809780bada159c7116023644bcc8ed890
MihaiAC <alexmihai.ciobanu@gmail.com>
2025-01-27 14:39:02 +0200

    Add Viewset and Router for the Recipe model.

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+"""Views for the recipe APIs."""
 
-# Create your views here.
+from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+from core.models import Recipe
+from recipe import serializers
+
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    """View for managing Recipe APIs."""
+
+    serializer_class = serializers.RecipeSerializer
+    queryset = Recipe.objects.all()
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [TokenAuthentication]
+
+    # We need to retrieve the Recipes only for the current user.
+    def get_queryset(self):
+        """Retrieve recipes for authenticated users."""
+        return self.queryset.filter(user=self.request.user).order_by("-id")
