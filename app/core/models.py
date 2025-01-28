0f7cbe553ce8eab86e560b7913c9f3375cdb2b99
MihaiAC <alexmihai.ciobanu@gmail.com>
2025-01-28 13:44:34 +0200

    Add Tag model.

--- app/core/models.py
+++ app/core/models.py
@@ -50,6 +50,19 @@ class User(AbstractBaseUser, PermissionsMixin):
     # REQUIRED_FIELDS = ["name"]
 
 
+class Tag(models.Model):
+    """Tag for filtering recipes."""
+
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.name
+
+
 class Recipe(models.Model):
     """Recipe object."""
 
@@ -62,6 +75,7 @@ class Recipe(models.Model):
     time_minutes = models.IntegerField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
     link = models.CharField(max_length=255, blank=True)
+    tags = models.ManyToManyField(Tag)
 
     def __str__(self):
         return self.title
