3a763c2f0ba50ef3fca6794155dc83b79f846a77
MihaiAC <alexmihai.ciobanu@gmail.com>
2025-01-29 13:07:11 +0200

    Add ingredient model.
    That doesn't really make sense so far, but good(?) practice.

--- app/core/models.py
+++ app/core/models.py
@@ -51,7 +51,7 @@ class User(AbstractBaseUser, PermissionsMixin):
 
 
 class Tag(models.Model):
-    """Tag for filtering recipes."""
+    """Model for filtering recipes."""
 
     name = models.CharField(max_length=255)
     user = models.ForeignKey(
@@ -64,7 +64,7 @@ class Tag(models.Model):
 
 
 class Recipe(models.Model):
-    """Recipe object."""
+    """Model for recipes."""
 
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
@@ -76,6 +76,19 @@ class Recipe(models.Model):
     price = models.DecimalField(max_digits=5, decimal_places=2)
     link = models.CharField(max_length=255, blank=True)
     tags = models.ManyToManyField(Tag)
+    ingredients = models.ManyToManyField("Ingredient")
 
     def __str__(self):
         return self.title
+
+
+# This model and the tag model don't really make sense -
+# why would you want the same ingredient/tag replicated across multiple users?
+class Ingredient(models.Model):
+    """Model for recipe ingredients."""
+
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
