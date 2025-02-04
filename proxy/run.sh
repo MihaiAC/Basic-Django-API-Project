fcae281be543489ee5384009322f5468f7540abe
MihaiAC <alexmihai.ciobanu@gmail.com>
2025-02-04 18:11:12 +0200

    Create nginx Dockerfile and configuration files.

---
+++ proxy/run.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Start the proxy service.
+
+set -e
+
+# Put our config file where nginx expects it and substitute the env
+# vars (at runtime).
+envsubst < /etc/nginx/default.conf.tpl > /etc/nginx/conf.d/default.conf
+
+# Run nginx in the foreground (since it is run in a separate Docker container).
+nginx -g 'daemon off';
