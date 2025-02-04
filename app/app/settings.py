2f7ea7155f4a9a855a591bb24199877206cdf828
MihaiAC <alexmihai.ciobanu@gmail.com>
2025-02-04 19:43:21 +0200

    Read env variables in settings.py.

--- app/app/settings.py
+++ app/app/settings.py
@@ -21,12 +21,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-f^f+-=ltjq#-7dooe%smy2qoo$@qd17_(c5k29#el)+p%#2&yk"
+SECRET_KEY = os.environ.get("SECRET_KEY", "changeme")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get("DEBUG", 0)))
 
+# Ensures that only those hosts can access the app.
 ALLOWED_HOSTS = []
+ALLOWED_HOSTS.extend(
+    filter(None, os.environ.get("ALLOWED_HOSTS", "").split(",")),
+)
 
 
 # Application definition
